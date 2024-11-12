11afd8c7d1983ec759d101ab9ed12e3c5a1d3a56
thathariss <hkhan.khan14007@gmail.com>
2024-11-13 01:55:23 +0530

    first commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+export default function Cart() {
+    
+    return (
+        <div className="cart">
+            <h2>Your Cart</h2>
+            <ul>
+                <li className="cart-item">your item</li>
+            </ul>
+        </div>
+    )
+}
